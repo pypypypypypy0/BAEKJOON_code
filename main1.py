fe2e65c37dffa06ba8ceb5781cd77106ed773bc4
파이썬 <151485043+pypypypypypy0@users.noreply.github.com>
2023-11-20 12:57:28 +0900

    Create main1.py

---
+++ main1.py
@@ -0,0 +1,7 @@
+echo "# BAEKJOON_code" >> README.md
+  git init
+  git add README.md
+  git commit -m "first commit"
+  git branch -M main
+  git remote add origin https://github.com/pypypypypypy0/BAEKJOON_code.git
+  git push -u origin main
